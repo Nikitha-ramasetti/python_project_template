955b7a7ff55dd46379f44284ea3ff6d027b08be7
Ramasetti <nikitha-shruthi.ramasetti@bam.de>
2020-07-28 17:42:23 +0200

    configure pandas_sphinx_theme

--- doc/conf.py
+++ doc/conf.py
@@ -122,13 +122,13 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {"logo_only": True}
+# html_theme_options = {"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -142,7 +142,7 @@ html_theme_options = {"logo_only": True}
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = "_static/logo.png"
+html_logo = "_static/pandas.svg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
